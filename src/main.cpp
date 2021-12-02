22844dae582de12e9dd1ec7b32d1abd6b08d054a
Pedro Luis Rodriguez <pedroluisrp1998@gmail.com>
2021-12-02 19:22:59 +0100

    Actor class

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,24 @@
+#include "libtcod.hpp"
+#include "Actor.hpp"
 
-#include <SDL.h>
-#include <libtcod.h>
+int main() {
+   Actor player (25,25,'@',TCODColor::blue);
 
-#include <cstdlib>
-
-int main(int argc, char** argv) {
-  auto params = TCOD_ContextParams{};
-  params.tcod_version = TCOD_COMPILEDVERSION;
-  params.argc = argc;
-  params.argv = argv;
-  params.vsync = 1;
-  params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
-  params.window_title = "Libtcod Template Project";
-
-  auto console = tcod::Console{80, 25};
-  params.console = console.get();
-
-  auto context = tcod::new_context(params);
-
-  // Game loop.
-  while (true) {
-    // Rendering.
-    console.clear();
-    tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
-    context->present(console);
-
-    // Handle input.
-    SDL_Event event;
-    SDL_WaitEvent(nullptr);
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          std::exit(EXIT_SUCCESS);
-          break;
-      }
-    }
-  }
-  return 0;
+   TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+   while ( !TCODConsole::isWindowClosed() ) {
+       TCOD_key_t key;
+       TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+       switch(key.vk) {
+           case TCODK_UP : player.y--; break;
+           case TCODK_DOWN : player.y++; break;
+           case TCODK_LEFT : player.x--; break;
+           case TCODK_RIGHT : player.x++; break;
+           default:break;
+       }
+       TCODConsole::root->clear();
+       //TCODConsole::root->putChar(player.x,player.y,player.);
+       player.render();
+       TCODConsole::flush();
+   }
+   return 0;
 }
